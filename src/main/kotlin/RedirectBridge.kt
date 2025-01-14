0bd23396a11cdcb55a3d0d4fe59dd8d21ae6dacb
urFate <georgiylakidon@gmail.com>
2025-01-14 15:21:27 +0300

    fix: bad request response

--- src/main/kotlin/RedirectBridge.kt
+++ src/main/kotlin/RedirectBridge.kt
@@ -1,9 +1,8 @@
 import com.google.gson.JsonParser
-import okhttp3.Headers
-import okhttp3.OkHttpClient
-import okhttp3.Request
+import okhttp3.*
 import okhttp3.RequestBody.Companion.toRequestBody
-import okhttp3.Response
+import java.net.URI
+import java.util.regex.Pattern
 
 
 class RedirectBridge {
@@ -12,60 +11,92 @@ class RedirectBridge {
     private val payloadJson: String = RedirectBridge::class.java.getResource("payload.json")!!.readText()
 
     fun execute(target: String): Response {
-        val headersObject = JsonParser.parseString(headersJson).asJsonObject
+        val headers = JsonParser.parseString(headersJson).asJsonObject.asMap().mapValues { it.value.asString }
         val payloadObject = JsonParser.parseString(payloadJson).asJsonObject
 
-        val headers = Headers.headersOf(*headersObject.asMap().map { "${it.key}: ${it.value}" }.toTypedArray())
+        val hostName = URI(target).host
 
         val request = Request.Builder()
             .url(target)
-            .headers(headers)
+            .apply {
+                headers.forEach { (key, value) -> addHeader(key, value) }
+            }
             .build()
 
         client.newCall(request).execute().use { response ->
+            val originCookies = response.headers("Set-Cookie").map { it.split(";")[0] }
+            val checkCookies = mutableListOf<String>()
+
             if (!response.isSuccessful) {
-                val srvRequest = Request.Builder()
-                    .url("$target/.well-known/ddos-guard/check?context=free_splash")
-                    .headers(headers)
-                    .build()
-                client.newCall(srvRequest).execute()
 
                 val checkRequest = Request.Builder()
                     .url("https://check.ddos-guard.net/check.js")
-                    .headers(headers)
+                    .apply {
+                        headers.forEach { (key, value) -> addHeader(key, value) }
+                        addHeader("Referer", "https://$hostName")
+                    }
                     .build()
 
                 client.newCall(checkRequest).execute().use { checkResponse ->
                     if (!checkResponse.isSuccessful) throw IllegalArgumentException("An error occurred on check request stage: ${checkResponse.code}")
+                    checkCookies.addAll(checkResponse.headers("Set-Cookie").map { it.split(";")[0] })
 
-                    val data = checkResponse.body!!.toString().split("'")
+                    val checkRequestBody = checkResponse.body!!.string()
 
-                    val firstCheckUrl = target + data[1]
-                    val secondCheckUrl = data[2]
+                    val pattern = Pattern.compile("'.*?'")
+                    val matcher = pattern.matcher(checkRequestBody)
+
+                    val data = ArrayList<String>()
+
+                    while (matcher.find()) {
+                        val quote = matcher.group()
+                        val length = quote.length
+                        data.add(quote.substring(1, length - 1))
+                    }
+
+                    val firstCheckUrl = "https://$hostName${data[0]}"
+                    val secondCheckUrl = data[1]
 
                     val firstCheckRequest = Request.Builder()
                         .url(firstCheckUrl)
-                        .headers(headers)
+                        .apply {
+                            headers.forEach { (key, value) -> addHeader(key, value) }
+                        }
                         .build()
                     val secondCheckRequest = Request.Builder()
                         .url(secondCheckUrl)
-                        .headers(headers)
+                        .apply {
+                            headers.forEach { (key, value) -> addHeader(key, value) }
+                        }
                         .build()
 
                     client.newCall(firstCheckRequest).execute()
                     client.newCall(secondCheckRequest).execute()
 
                     val postRequest = Request.Builder()
-                        .url("$target/.well-known/ddos-guard/mark/")
+                        .url("https://$hostName/.well-known/ddos-guard/mark/")
                         .post(payloadObject.toString().toRequestBody())
-                        .headers(headers)
+                        .apply {
+                            headers.forEach { (key, value) -> addHeader(key, value) }
+                            addHeader("Referer", target)
+                            addHeader("Origin", "https://$hostName")
+                            addHeader("Cookie", originCookies.joinToString(";") + checkCookies.joinToString(";"))
+                        }
                         .build()
 
                     client.newCall(postRequest).execute().use { postResponse ->
-                        if (!postResponse.isSuccessful) throw IllegalArgumentException("An error occurred on post request stage: ${checkResponse.code}")
-                        client.newCall(request).execute().use { endResponse ->
-                            return endResponse
-                        }
+                        if (!postResponse.isSuccessful) throw IllegalArgumentException("An error occurred on post request stage: ${postResponse.code}")
+
+                        val endRequest = Request.Builder()
+                            .url(target)
+                            .apply {
+                                headers.forEach { (key, value) -> addHeader(key, value) }
+                                addHeader("Referer", target)
+                                addHeader("Cookie", originCookies.joinToString(";") + checkCookies.joinToString(";"))
+                            }
+                            .build()
+
+                        return client.newCall(endRequest).execute()
                     }
                 }
             } else {
